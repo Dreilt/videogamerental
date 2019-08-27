c3a763f6cbbee897e594a8cfe769118462e67a70
Patryk <w57014@student.wsiz.rzeszow.pl>
2019-08-27 12:29:37 +0200

    Initial commit

---
+++ src/main/java/pl/patryk/videogamerental/VideogamerentalApplication.java
@@ -0,0 +1,13 @@
+package pl.patryk.videogamerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamerentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideogamerentalApplication.class, args);
+    }
+
+}
